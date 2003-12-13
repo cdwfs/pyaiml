5d4cb6b00b4aca02865b87cf69f8382b82ae4edf
cort <cort>
2003-12-13 01:14:46 +0000

    Bumped version to 0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import glob
 package_prefix = "Lib/site-packages/aiml"
 
 setup(name="PyAIML",
-    version="0.2",
+    version="0.3",
     author="Cort Stratton",
     author_email="cort@users.sourceforge.net",
     description="Python AIML Interpreter",
