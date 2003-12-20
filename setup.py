a42c2a8f1a30cef3aba54b180af9c64561a35288
cort <cort>
2003-12-20 19:29:56 +0000

    Added more metadata to facilitate registering with the Python Package Index.

--- setup.py
+++ setup.py
@@ -8,8 +8,24 @@ setup(name="PyAIML",
     version="0.5",
     author="Cort Stratton",
     author_email="cort@users.sourceforge.net",
-    description="Python AIML Interpreter",
+    maintainer="Cort Stratton",
+    maintainer_email="cort@users.sourceforge.net",
+    
+    description="An interpreter package for AIML, the Artificial Intelligence Markup Language",
+    long_description="""PyAIML implements an interpreter for AIML, the Artificial Intelligence
+Markup Language developed by Dr. Richard Wallace of the A.L.I.C.E. Foundation.
+It can be used to implement a conversational AI program.""",
     url="http://pyaiml.sourceforge.net/",
+    platforms=["any"],
+    classifiers=["Development Status :: 3 - Alpha",
+                 "Environment :: Console",
+                 "Intended Audience :: Developers",
+                 "Programming Language :: Python",
+                 "Operating System :: OS Independent",
+                 "Topic :: Communications :: Chat",
+                 "Topic :: Scientific/Engineering :: Artificial Intelligence"
+                 ],
+      
     packages=["aiml"],
     data_files=[
         (package_prefix, glob.glob("aiml/self-test.aiml")),
