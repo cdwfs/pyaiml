dcbb775bffe86a7bec712318396d61518d2a5fbc
cort <cort>
2004-01-03 20:30:59 +0000

    Exposed AimlParser and AimlParserError classes

--- aiml/__init__.py
+++ aiml/__init__.py
@@ -1,2 +1,13 @@
 __all__ = []
+
+# The Kernel class is the only class most implementations should need.
 from Kernel import Kernel
+
+# AimlParser is a subclass of xml.sax.handler.ContentHandler, suitable
+# for passing as an argument to xml.sax.parse() and friends.
+# AimlParserError is the exception raised by the AimlParser class
+# when it finds an error.
+# These classes are only useful for parsing (NOT interpreting) raw AIML
+# (for example, to implement an AIML validator).
+from AimlParser import AimlParser, AimlParserError
+
