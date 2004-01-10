16a7b020a591693d093b89f905a131ca635904f1
cort <cort>
2004-01-10 04:55:16 +0000

    Removed AimlParserError from public view.  Script now finds all errors in input files, not just the first one.  Also added comments.

--- aimlvalidate.py
+++ aimlvalidate.py
@@ -12,23 +12,38 @@ import sys
 import xml.sax
 
 if __name__ == "__main__":
+    # Need input file(s)!
     if len(sys.argv) < 2:
         print __doc__
+        sys.exit(2)
+
+    # Iterate over input files    
     validCount = 0
     docCount = 0
     for arg in sys.argv[1:]:
+        # Input files can contain wildcards; iterate over matches
         for f in glob.glob(arg):
+            # Create a new AimlParser for each file.
             handler = aiml.AimlParser()
             docCount += 1
+            print "%s:" % f
             try:
-                print "%s:" % f
+                # Attempt to parse the file.
                 xml.sax.parse(f, handler)
-                validCount += 1
-                print "PASSED\n"
-            except aiml.AimlParserError, msg:
-                print "ERROR: %s\n" % msg
+                # Check the number of parse errors.
+                if handler.getNumErrors() == 0:    
+                    validCount += 1
+                    print "PASSED\n"
+                else:
+                    print "FAILED\n"
             except xml.sax.SAXParseException, msg:
-                print "ERROR: %s\n" % msg
+                # These errors occur if the document does not contain
+                # well-formed XML (e.g. open or unbalanced tags).  If
+                # they occur, parsing the whole document is aborted
+                # immediately.
+                print "FATAL ERROR: %s\n" % msg
+                
+    # Print final results
     print "%d out of %d documents are AIML 1.0.1 compliant." % (validCount, docCount)
     if docCount == validCount:
         print "Congratulations!"
